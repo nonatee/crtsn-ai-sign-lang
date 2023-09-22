49b5765cae7f9d3cebdf63b3877b5874e0fba510
ibrahimBal0109 <145055045+ibrahimBal0109@users.noreply.github.com>
2023-09-22 19:16:14 +0300

    Update HandRecogClass.py

--- HandRecogClass.py
+++ HandRecogClass.py
@@ -4,12 +4,27 @@ import time
 
 
 class HandRecognnition:
-    def __init__(self):
+    def __init__(self, static_image_mode=False, max_num_hands=2, model_complexity=1, min_detection_confidence=0.5,
+                 min_tracking_confidence=0.5):
+
+        self.static_image_mode = static_image_mode
+        self.max_num_hands = max_num_hands
+        self.model_complexity = model_complexity
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+
         self.cap = cv.VideoCapture(0)
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands()
+        self.hands = self.mpHands.Hands(self.static_image_mode,
+                                        self.max_num_hands,
+                                        self.model_complexity,
+                                        self.min_detection_confidence,
+                                        self.min_tracking_confidence)
+
         self.mpDraw = mp.solutions.drawing_utils
+
+        self.infos = {}
 
         self.previous_time = 0
         self.current_time = 0
@@ -21,7 +36,18 @@ class HandRecognnition:
         self.previous_time = self.current_time
         cv.putText(self.frame, "FPS: "+str(int(self.fps)), (x, y), cv.FONT_ITALIC, 0.5, (0, 0, 0), thickness=1)
 
-    def main(self):
+    def get_info(self, handlms):
+        for id, landmark in enumerate(handlms.landmark):
+            height, width, channels = self.frame.shape
+            cx, cy = int(landmark.x * width), int(landmark.y * height)
+            self.infos = {
+            "ID": id,
+            "x_axis" : cx,
+            "y_axis" : cy
+            }
+            print(self.infos)
+
+    def main(self, draw=True, infos=True):
         is_true, self.frame = self.cap.read()
         self.imgRGB = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
         results = self.hands.process(self.imgRGB)
@@ -29,13 +55,18 @@ class HandRecognnition:
 
         if results.multi_hand_landmarks:
             for handLms in results.multi_hand_landmarks:
-                for id, landmark in enumerate(handLms.landmark):
-                    height, width, channels = self.frame.shape
-                    cx, cy = int(landmark.x * width), int(landmark.y * height)
-                    print(id, cx, cy)
-                self.mpDraw.draw_landmarks(self.frame, handLms, self.mpHands.HAND_CONNECTIONS)
 
-        self.fps_write(10,30)
+                if draw & infos:
+                    self.get_info(handLms)
+                    self.mpDraw.draw_landmarks(self.frame, handLms, self.mpHands.HAND_CONNECTIONS)
+
+                elif draw:
+                    self.mpDraw.draw_landmarks(self.frame, handLms, self.mpHands.HAND_CONNECTIONS)
+
+                elif infos:
+                    self.get_info(handLms)
+
+        self.fps_write(10, 30)
 
         cv.imshow('SignLanguageAi', self.frame)
 
@@ -45,5 +76,5 @@ Main = HandRecognnition()
 if __name__ == '__main__':
     while True:
         Main.main()
-        if cv.waitKey(20) & 0xFF==ord('q'):
+        if cv.waitKey(20) & 0xFF == ord('q'):
             break
